f3b2a4315ccaf18cc3650cc99cb23a3248a5fae8
Paulamariel1979 <paulamarieldefigueiredo@gmail.com>
2022-09-27 13:37:49 +0200

    Calculadorabostrap

--- practicando.js
+++ practicando.js
@@ -1,6 +1,14 @@
 const peliculas = new Array(2);
 
-let peliculas1 = ["star wars", "matrix", "back to",]
+let peliculas1 = ["star wars", "matrix", "back to"]
+
+peliculas.forEach((elemento, indice, peli) => {
+    console.log({ elemento, indice, peli })
+})
+
+for (let i = 0; i < peliculas.length; i++) {
+
+}
 
 /* const peli =[
     "Juan",
@@ -10,4 +18,4 @@ let peliculas1 = ["star wars", "matrix", "back to",]
     [10, 11, 12, 13, [100, "hola"]]
     ];
 
-    console.log(peli[4][4] [1]);
+    console.log(peli[4][4] [1])/*
